d6dda6a97aed3b8ea87fae54ba5a6a79985f1ab8
akahana-1 <h163341@edu.tut.ac.jp>
2017-04-17 13:27:24 +0900

    update

--- pickit/pickit.py
+++ pickit/pickit.py
@@ -4,26 +4,25 @@
 import pickle
 
 class PickIt(object):
-	def __init__(self, pickle_file_name, n = -1):
-		self.fp = open(pickle_file_name, "rb")
-		self.loadmax = n
+    def __init__(self, pickle_file_name, n = -1):
+        self.fp = open(pickle_file_name, "rb")
+        self.loadmax = n
 
-	def __iter__(self, ):
-		self.idx = self.loadmax
-		return self
+    def __iter__(self, ):
+        self.idx = self.loadmax
+        return self
 
-	def __next__(self, ):
-		ret = None
-		if self.idx != 0:
-			try:
-				ret = pickle.load(self.fp)
-				self.idx -= 1
-			except:
-				self.fp.close()
-				raise StopIteration
-		else:
-			raise StopIteration
-		return ret
+    def __next__(self, ):
+        ret = None
+        if self.idx != 0:
+            try:
+                ret = pickle.load(self.fp)
+                self.idx -= 1
+            except:
+                self.fp.close()
+        else:
+            raise StopIteration
+        return ret
 
-	def __del__(self, ):
-		self.fp.close()
+    def __del__(self, ):
+        self.fp.close()
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-version = '1.0'
+version = '1.01'
 
 setup(
 		name='pickit',
